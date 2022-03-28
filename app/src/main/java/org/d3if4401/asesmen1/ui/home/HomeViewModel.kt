a294b952c783b37c4e6e775ca61e0afde3247494
Dwiko Indrawansyah <wesibrani@gmail.com>
2022-03-28 23:03:13 +0700

    Initial commit

---
+++ app/src/main/java/org/d3if4401/asesmen1/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package org.d3if4401.asesmen1.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
---
+++ app/src/main/java/org/d3if4401/asesmen1/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package org.d3if4401.asesmen1.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
